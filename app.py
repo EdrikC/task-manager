71be04e5c67ad9c1f848ee8ec1bacd3f4d5f86d1
edrikreyes <edrik.reyes9@gmail.com>
2023-08-13 17:28:56 -0400

    Set up database with environmental variable to hide db connection key

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from database import load_tasks
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def main_page():
     return render_template('index.html')
+
+
+@app.route('/task-page')
+def add_task():
+    return render_template('task-page.html', tsk=load_tasks())
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,3 +1,25 @@
-import sqlalchemy
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
 
-print(sqlalchemy.__version__)
+
+load_dotenv()
+db_connect_string = os.getenv('DB_STR')
+
+
+# Creating the engine.
+engine = create_engine(db_connect_string, connect_args={
+    "ssl": {
+        "ca": "/etc/ssl/cert.pem",
+    }
+})
+
+
+def load_tasks():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from tasks"))
+        ls_tasks = []
+        for row in result:
+            row_as_dict = row._mapping
+            ls_tasks.append(row_as_dict)
+        return ls_tasks
