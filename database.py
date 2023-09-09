d58a45a287ff8dbf41b009c565c927ea853d1b91
edrikreyes <edrik.reyes9@gmail.com>
2023-09-09 14:11:58 -0400

    Render compatibility change 8

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ load_dotenv()
 db_connect_string = os.getenv('DB_STR')
 
 # Creating the engine.
-engine = create_engine(db_connect_string, connect_args={"ssl_ca": "ca.pem"})
+engine = create_engine(db_connect_string, connect_args={"ssl_ca": ""})
 
 
 # Loads the tasks rendered in task-page.html given the user's id. (Returned as a list of dicts)
